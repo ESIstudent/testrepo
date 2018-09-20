12500b5f290d607219602ba3dacb7fed202d2990
ESIstudent <7399esi@gmail.com>
2018-09-20 10:03:47 -0100

    Create primary_colors.php

---
+++ primary_colors.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Primary Colors</title>
+</head>
+<body>
+<ul>
+<li><?PHP echo "Yellow"; ?></li>
+<li><?PHP echo "Red   "; ?></li>
+<li><?PHP echo "Blue  "; ?></li>
+</ul>
+</body>
+</html>
